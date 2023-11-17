0baa5221380aeef53b6255aa8da904b2352c9ec0
Joshua Muiruri <psyjm27@bann.cs.nott.ac.uk>
2023-11-17 15:07:42 +0000

    completed sim1

---
+++ simulator1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include "coursework.h"
+
+
+int main(){
+    Process *process1;
+    int iPID = 1;
+    long int response, turnaro;
+    bool bsim = false;
+
+    process1 = generateProcess(iPID);
+    printf("GENERATOR - CREATED: [PID = %d, Priority = %d, InitialBurstTime = %d, RemainingBurstTime = %d]\n",process1->iPID,process1->iPriority, process1->iBurstTime, process1->iRemainingBurstTime);
+    while (process1->iState != TERMINATED){
+        runPreemptiveProcess(process1, bsim);
+        printf("SIMULATOR - CPU 0: RR [PID = %d, Priority = %d, InitialBurstTime = %d, RemainingBurstTime = %d]\n" ,process1->iPID ,process1->iPriority, process1->iBurstTime, process1->iRemainingBurstTime);
+    }
+    response = getDifferenceInMilliSeconds(process1->oTimeCreated, process1->oFirstTimeRunning);
+    turnaro = getDifferenceInMilliSeconds(process1->oFirstTimeRunning, process1->oLastTimeRunning);
+    printf("TERMINATOR - TERMINATED: [PID = %d, ResponseTime = %ld, TurnAroundTime = %ld]\n",process1->iPID, response, turnaro);
+    destroyProcess(process1);
+
+}
