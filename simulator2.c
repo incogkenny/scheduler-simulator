583a353e88ef1413a13b4908a02576697c024f7f
Joshua Muiruri <psyjm27@bann.cs.nott.ac.uk>
2023-12-23 16:02:50 +0000

    Merge branch 'main' of https://projects.cs.nott.ac.uk/comp2007/2023-2024/psyjm27_coursework into main

--- simulator2.c
+++ simulator2.c
@@ -74,3 +74,4 @@ int main(){
     printf("TERMINATION DAEMON: [Average Response Time = %lf, Average Turn Around Time = %lf]\n", (mean_r/NUMBER_OF_PROCESSES), (mean_tu/NUMBER_OF_PROCESSES));
     
 }
+
